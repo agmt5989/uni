a6e6dea710eb234f5c066ba25b392ace8905d8e3
Michael Ajala <agmt5989@gmail.com>
2019-01-22 18:56:53 +0100

    couch from the examples

--- couch.js
+++ couch.js
@@ -1,16 +1,49 @@
-var couchbase = require('couchbase');
-var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
-// For Couchbase > 4.5 with RBAC Auth
-// cluster.authenticate('root', 'admins')
-var bucket = cluster.openBucket('default');
+var couchbase = require("./lib/couchbase.js"),
+    http = require("http"),
+    fs = require("fs"),
+    util = require("util");
 
-bucket.upsert('testdoc', {name:'Frank'}, function(err, result) {
-  if (err) throw err;
+var port = 8080;
 
-  bucket.get('testdoc', function(err, result) {
-    if (err) throw err;
+var configFilename = 'config.json';
+if (fs.existsSync(configFilename)) {
+  config = JSON.parse(fs.readFileSync(configFilename));
+} else {
+  console.log(configFilename + " not found. Using default");
+  config = { };
+}
 
-    console.log(result.value);
-    // {name: Frank}
-  });
+bucket = new couchbase.Connection(config, function(err) {
+  if (err) {
+    // For some reason we failed to make a connection to the
+    // Couchbase cluster.
+    throw err;
+  }
 });
+
+http.createServer(function(req, resp) {
+  bucket.get("hitcount", function(err, result) {
+    var doc = result.value;
+    if (!doc) {
+      doc = {count:0};
+    }
+
+    doc.count++;
+
+    resp.writeHead(200);
+    console.log("hits", doc.count);
+
+    bucket.set("hitcount", doc, {}, function(err) {
+      if (err) {
+        console.warn("Failed to store hit counter: " +
+            util.inspect(err));
+        resp.end("<p>Internal server error. " +
+            "Failed to store:</p><pre>" + util.inspect(err) +
+            "</pre>");
+      } else {
+        resp.end('<p>The server has seen '+doc.count+' hits</p>');
+      }
+    });
+  })
+}).listen(port);
+console.log("listening on http://localhost:" + port);
