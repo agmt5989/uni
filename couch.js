597435ab2f8c5b4b3f19f260187928b13602f90d
Michael Ajala <agmt5989@gmail.com>
2019-01-22 16:48:05 +0100

    couch and the watchers

--- couch.js
+++ couch.js
@@ -1,5 +1,5 @@
 var couchbase = require('couchbase');
-// var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
 // For Couchbase > 4.5 with RBAC Auth
 cluster.authenticate('root', 'admins')
 var bucket = cluster.openBucket('default');
